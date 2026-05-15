15a7d9adba2a962602553ae61d90cced061a5ca8
James Hatherley <jameshatherley@icloud.com>
2026-05-15 12:22:17 +0100

    update website

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>My Website</h1>
-      <p>Welcome to my first Next.js site</p>
+      <p>Welcome to my first Next.js site I am excited</p>
     </main>
   );
 }
